3c069a013ba039e1c49735f863cd03d0c75f8f68
deribew <deribewsoftware@gmail.com>
2024-01-12 09:52:17 +0300

    Rating on Course added

--- app/course/[id]/page.tsx
+++ app/course/[id]/page.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/footer/footer";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { CourseContent } from "./courseContent";
+import Rating from "./rating/rating";
 
 
 const Course = () => {
@@ -28,10 +29,10 @@ const Course = () => {
   }, []);
   const {id}=useParams()
   return ( <div>
-    <div className="flex justify-center min-h-screen p-10 relative ">
-    <div className="flex justify-center items-center   min-h-screen  w-7/12  ">
-    <div className="flex flex-col gap-10 p-10 ">
-      <h1 className="text-3xl font-bold mt-4">
+    <div className="md:flex justify-center md:min-h-screen md:p-10 relative ">
+    <div className="md:flex justify-center items-center   min-h-screen  w-full md:w-7/12  ">
+    <div className="flex flex-col gap-10 p-2 md:p-10 ">
+      <h1 className="text-3xl font-bold md:mt-4">
         Course Title {id}
       </h1>
       <Container childern={ <div className="p-3">Global Promotional Products Company
@@ -78,33 +79,12 @@ Salary range: $110,000 to $160,000 per yearGlobal Promotional Products Company
 Hybrid, Atlanta, GA Area - Full Time / Direct Hire</div>}/>
        
       
-      <p>Global Promotional Products Company
-Hybrid, Atlanta, GA Area - Full Time / Direct Hire
-The Hired Guns are seeking a proactive, results-driven Print-On-Demand Sales Director to lead the POD business for a startup nestled within a $2-billion promotional products company. Note, this is a hybrid role in the Atlanta, Georgia area. You must be willing to go to the office in-person, and to relocate if you’re not currently living in the region.
-Salary range: $110,000 to $160,000 per yearGlobal Promotional Products Company
-Hybrid, Atlanta, GA Area - Full Time / Direct Hire
-The Hired Guns are seeking a proactive, results-driven Print-On-Demand Sales Director to lead the POD business for a startup nestled within a $2-billion promotional products company. Note, this is a hybrid role in the Atlanta, Georgia area. You must be willing to go to the office in-person, and to relocate if you’re not currently living in the region.
-Salary range: $110,000 to $160,000 per yearGlobal Promotional Products Company
-Hybrid, Atlanta, GA Area - Full Time / Direct Hire
-The Hired Guns are seeking a proactive, results-driven Print-On-Demand Sales Director to lead the POD business for a startup nestled within a $2-billion promotional products company. Note, this is a hybrid role in the Atlanta, Georgia area. You must be willing to go to the office in-person, and to relocate if you’re not currently living in the region.
-Salary range: $110,000 to $160,000 per yearvGlobal Promotional Products Company
-Hybrid, Atlanta, GA Area - Full Time / Direct Hire
-The Hired Guns are seeking a proactive, results-driven Print-On-Demand Sales Director to lead the POD business for a startup nestled within a $2-billion promotional products company. Note, this is a hybrid role in the Atlanta, Georgia area. You must be willing to go to the office in-person, and to relocate if you’re not currently living in the region.
-Salary range: $110,000 to $160,000 per yearGlobal Promotional Products Company
-Hybrid, Atlanta, GA Area - Full Time / Direct Hire
-The Hired Guns are seeking a proactive, results-driven Print-On-Demand Sales Director to lead the POD business for a startup nestled within a $2-billion promotional products company. Note, this is a hybrid role in the Atlanta, Georgia area. You must be willing to go to the office in-person, and to relocate if you’re not currently living in the region.
-Salary range: $110,000 to $160,000 per yearGlobal Promotional Products Company
-Hybrid, Atlanta, GA Area - Full Time / Direct Hire
-The Hired Guns are seeking a proactive, results-driven Print-On-Demand Sales Director to lead the POD business for a startup nestled within a $2-billion promotional products company. Note, this is a hybrid role in the Atlanta, Georgia area. You must be willing to go to the office in-person, and to relocate if you’re not currently living in the region.
-Salary range: $110,000 to $160,000 per yearGlobal Promotional Products Company
-Hybrid, Atlanta, GA Area -
-The Hired Guns are seeking a proactive, results-driven Print-On-Demand Sales Director to lead the POD business for a startup nestled within a $2-billion promotional products company. Note, this is a hybrid role in the Atlanta, Georgia area. You must be willing to go to the office in-person, and to relocate if you’re not currently living in the region.
-Salary range: $110,000 to $160,000 per year</p>
+     <Rating/>
     </div>
     </div>
 
-    <div className="flex justify-center items-center  w-3/12 px-4 ">
-      <div className={`  overflow-y-auto overflow-x-hidden  ${isScroll? "":"top-10 fixed w-3/12 px-8"}`}>
+    <div className="flex justify-center items-center  w-full md:w-3/12 p-2 md:px-4 ">
+      <div className={`  overflow-y-auto overflow-x-hidden  ${isScroll? "":"top-10 md:fixed md:w-3/12 md:px-8"}`}>
       <div className="flex flex-col overflow-y-auto overflow-x-hidden   gap-10 ">
       <h4>Course Content</h4>
      <Container
--- app/course/[id]/rating/rating.tsx
+++ app/course/[id]/rating/rating.tsx
@@ -1,16 +1,34 @@
 import Container from "@/components/container/container";
-
+import { IoIosStar } from "react-icons/io";
 const Rating = () => {
+
+  const ratingData=[
+    {id:1},
+    {id:2},
+    {id:3},
+    {id:4},
+    {id:5},
+    {id:6},
+  ]
   return (  <Container childern={
-    <div className="flex justify-between px-2">
-      <div className="flex justify-center">
-        <h1 className="text-3xl font-bold">4.5</h1>
+    <div className="flex justify-center md:px-2 w-full  flex-col md:flex-row">
+      <div className="flex justify-center items-center w-full md:w-1/4 sm:py-10 ">
+       <div className="border border-teal-400 border-[1.5px]   p-10 m-4 rounded-[5px]">
+       <h1 className="text-3xl md:text-6xl text-teal-500 font-bold">4.5</h1>
+       </div>
       </div>
-      <div className="flex flex-col gap-4 p-1">
-<div className="flex">
-  1 star <div className="h-2 w-full bg-teal-400"></div>
-  10%
+      <div className="flex flex-col justify-center  p-1 w-full md:w-3/4">
+{ratingData.map((rating)=>{
+  return <div key={rating.id} className="flex w-full px-4">
+  <span className="flex ">
+    <span className="mb-1 px-1 text-lg font-medium">{rating.id-1} </span>
+    <IoIosStar size={24} className={`${rating.id-1? "text-teal-400":"text-slate-200"}`}/></span>
+     <div className="h-2 m-2 w-full bg-slate-200">
+     <div className={`h-full  bg-teal-400  rounded-r-[5px]`} style={{width:`${(rating.id-1)*100/5}%`}}></div>
+     </div>
+     {(rating.id-1)*100/5}%
 </div>
+})}
       </div>
     </div>
   }/>
