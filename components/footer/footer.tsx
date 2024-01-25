6d915e1d98124a7bde8e9f7464a3a111b074b446
deribew <deribewsoftware@gmail.com>
2024-01-25 13:54:14 +0300

    footer  added

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return ( 
 
-    <footer className="bg-white dark:bg-gray-900">
+    <footer className="bg-white dark:bg-gray-800">
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
             <div className="md:flex md:justify-between">
               <div className="mb-6 md:mb-0">
